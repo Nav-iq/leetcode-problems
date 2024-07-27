30d6bb6ce7b352d60b0833b8e20a3fd468d338fd
Naveed <104513717+Nav-iq@users.noreply.github.com>
2024-07-27 13:35:21 +0000

    Sync LeetCode submission - Counter II (javascript)

---
+++ my-folder/problems/counter_ii/solution.js
@@ -0,0 +1,18 @@
+function createCounter(init) {
+  let currentValue = init;
+
+  return {
+    increment() {
+      currentValue++;
+      return currentValue;
+    },
+    decrement() {
+      currentValue--;
+      return currentValue;
+    },
+    reset() {
+      currentValue = init;
+      return currentValue;
+    }
+  };
+}
